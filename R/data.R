f8b636f671274556b3c51c76413f53dcf99d81bc
your name <eli.pousson@gmail.com>
2024-03-31 17:18:56 -0400

    Add legistar_clients reference

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Legistar Client Reference
+#'
+#' A data frame of identified Legistar client sites. Last updated 2024-03-31.
+#'
+#' @format A data frame with 180 rows and 2 variables:
+#' \describe{
+#'   \item{\code{client}}{Client string}
+#'   \item{\code{api}}{logical If `TRUE`, client returned an API response}
+#'}
+"legistar_clients"
