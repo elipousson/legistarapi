91487cfb3c20ad99531df582338667f1b8574e86
your name <eli.pousson@gmail.com>
2024-03-31 22:36:08 -0400

    Expand legistar_clients data

--- R/data.R
+++ R/data.R
@@ -1,8 +1,10 @@
 #' Legistar Client Reference
 #'
-#' A data frame of identified Legistar client sites. Last updated 2024-03-31.
+#' A data frame of identified Legistar client sites tested to check if client
+#' responded to an API request with [legistar_get_bodies()]. Last updated
+#' 2024-03-31.
 #'
-#' @format A data frame with 180 rows and 2 variables:
+#' @format A data frame with 286 rows and 2 variables:
 #' \describe{
 #'   \item{\code{client}}{Client string}
 #'   \item{\code{api}}{logical If `TRUE`, client returned an API response}
