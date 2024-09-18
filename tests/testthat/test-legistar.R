60fd69d147a44280799307e7b59c724b532f1aba
Eli Pousson <eli.pousson@gmail.com>
2024-09-17 22:34:21 -0400

    Fix legistar_body test

--- tests/testthat/test-legistar.R
+++ tests/testthat/test-legistar.R
@@ -1,5 +1,5 @@
 test_that("legistar works", {
   legistar_body <- legistar(client = "baltimore", top = 10)
   expect_s3_class(legistar_body, "data.frame")
-  expect_identical(nrow(legistar_body), 10)
+  expect_identical(nrow(legistar_body), as.integer(10))
 })
